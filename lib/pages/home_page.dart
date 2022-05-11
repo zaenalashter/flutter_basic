5c4d76f9076526fd0ea2bc6fcc34321d70e1a9b3
zaenalashter <santrialit77@gmail.com>
2022-05-12 04:46:58 +0700

    Basic5-ListView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,97 +8,102 @@ class HomePage extends StatelessWidget {
       backgroundColor: backgroundColor,
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                'Profile Picture',
-                style: TextStyle(
-                    color: primaryColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Image.asset(
-                'images/primary.png',
-                width: 140,
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Text(
-                'Muhamad Zainal',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-              ),
-              SizedBox(
-                height: 4,
-              ),
-              Text(
-                'UX Designer',
-                style: TextStyle(
-                  color: greyColor,
-                  fontSize: 16,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-              SizedBox(
-                height: 70,
-              ),
-              Wrap(
-                spacing: 38,
-                runSpacing: 40,
-                children: [
-                  Image.asset(
-                    'images/item1.png',
-                    width: 80,
+                Text(
+                  'Profile Picture',
+                  style: TextStyle(
+                      color: primaryColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  'images/primary.png',
+                  width: 140,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Muhamad Zainal',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'UX Designer',
+                  style: TextStyle(
+                    color: greyColor,
+                    fontSize: 16,
                   ),
-                  Image.asset(
-                    'images/item2.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'images/item3.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'images/item4.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'images/item5.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'images/item6.png',
-                    width: 80,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 70,
-              ),
-              Container(
-                width: 224,
-                height: 54,
-                child: RaisedButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(1160),
-                  ),
-                  color: whiteColor,
-                  onPressed: () {},
-                  child: Text(
-                    'Update Profile',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Wrap(
+                  spacing: 38,
+                  runSpacing: 40,
+                  children: [
+                    Image.asset(
+                      'images/item1.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'images/item2.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'images/item3.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'images/item4.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'images/item5.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'images/item6.png',
+                      width: 80,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 150,
+                ),
+                Container(
+                  width: 224,
+                  height: 54,
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(1160),
+                    ),
+                    color: whiteColor,
+                    onPressed: () {},
+                    child: Text(
+                      'Update Profile',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 76,
+                ),
+              ],
+            ),
           ),
         ),
       ),
